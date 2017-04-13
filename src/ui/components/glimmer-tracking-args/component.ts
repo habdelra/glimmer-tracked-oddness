b7092a5a1ced492f554bec7323c61b25d9689795
Hassan Abdel-Rahman <hassan.abdelrahman@mhelabs.com>
2017-04-13 16:48:35 -0400

    odd issue with setting tracked property

--- src/ui/components/glimmer-tracking-args/component.ts
+++ src/ui/components/glimmer-tracking-args/component.ts
@@ -1,24 +1,10 @@
 import Component, { tracked } from "@glimmer/component";
 
 export default class GlimmerTrackingArgs extends Component {
-
   @tracked
-  model: any = {
-    foo: "bar",
-    bleep: "blurp"
-  };
-
-  @tracked ("model")
-  get stringifiedModel() {
-    return JSON.stringify(this.model);
-  }
+  foo: boolean = false;
 
   didInsertElement() {
-    setTimeout(() => {
-      this.model = {
-        ...this.model,
-        foo: "bloop"
-      };
-    }, 1000);
+    this.foo = true;
   }
 }
